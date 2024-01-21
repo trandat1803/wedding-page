62f53362b21cedca0f17f983232b8aed98de4749
trandat1803 <quocdat@MacprocuaDatne.local>
2024-01-21 13:31:07 +0700

    re-update repo

---
+++ js.js
@@ -0,0 +1,10 @@
+// swiper slider
+var swiper = new Swiper(".mySwiper", {
+    effect: "cards",
+    grabCursor: true,
+    loop: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+  });
